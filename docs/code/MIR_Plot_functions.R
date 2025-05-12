f872823194de467043876b91b5e3c9098e338902
rob-harper <robby1112@gmail.com>
2025-05-12 14:29:52 -0400

    New function for plotting LF

--- docs/code/MIR_Plot_functions.R
+++ docs/code/MIR_Plot_functions.R
@@ -95,3 +95,37 @@ MIR_LF <- function(df, spp, bin_size, yrs = NULL, spp_name) {
     plot_bins(x = y, ttle = paste0(spp_name, " ", yrs), bin_size = bin_size)
 
 }
+
+MIR_LF_yr <- function(df, spp, bin_size, yrs = NULL, spp_name, category, custom_title = NULL) {
+  x <- getDomainLengthFrequency(df, species = spp, merge_protected = F) %>%
+    group_by(YEAR, SPECIES_CD, protected_status) %>%
+    nest() %>%
+    mutate(Lf = map(data, ~ .x %>%
+                      data.frame() %>%
+                      full_join(., data.frame(length_class = seq(1,max(.$length_class),0.5))) %>%
+                      select(length_class, frequency) %>%
+                      replace(., is.na(.), 0) %>%
+                      mutate(bin = as.numeric(cut(length_class, seq(0, max(length_class) + 5, bin_size)))) %>%
+                      arrange(length_class) %>%
+                      group_by(bin) %>%
+                      summarise(freq = sum(frequency)))) %>%
+    unnest(Lf) %>%
+    select(YEAR, SPECIES_CD, protected_status, bin, freq) %>%
+    ungroup() %>%
+    mutate(value = freq, variable = if_else(protected_status == 1, "M:IR", "open"))
+
+  y <- x %>%
+    filter(YEAR %in% yrs, variable == category) %>%  # Filter by category (M:IR or open)
+    select(YEAR, SPECIES_CD, variable, bin, value) %>%
+    pivot_wider(names_from = bin, values_from = value, values_fill = 0) %>%
+    pivot_longer(!c(YEAR, SPECIES_CD, variable), names_to = "bin", values_to = "value") %>%
+    mutate(bin = as.numeric(bin))
+
+  # If no custom title is provided, default to species name and category
+  if (is.null(custom_title)) {
+    custom_title <- paste0(spp_name, " - ", category)
+  }
+
+  # Pass spp_name, category, and custom_title to plot_bins_yr
+  plot_bins_yr(x = y, ttle = custom_title, bin_size = bin_size, category = category, spp_name = spp_name)
+}
